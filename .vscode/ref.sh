acc2262736ee796271c0456eb688dafa28bc59ea
LeonardoJaques <jaques.projetos@outlook.com>
2021-04-05 15:52:14 -0300

    First Commit

---
+++ .vscode/ref.sh
@@ -0,0 +1,27 @@
+create in root of project a file with name => eslinttc.json
+{
+  "plugins": ["testing-library", "jest-dom"],
+  "extends": [
+    "react-app",
+    "react-app/jest",
+    "plugin:testing-library/recommended",
+    "plugin:testing-library/react",
+    "plugin:jest-dom/recommended"
+  ]
+}
+create a folder in root of project with name => .vscode
+create a file in .vscode with name => settings.json
+{
+  "eslint.options": {
+    "configFile": ".eslintrc.json"
+  },
+  "eslint.validate": ["javascript", "javascriptreact"],
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true
+  }
+}
+
+install: npm i -D eslint-plugin-testing-library eslint-plugin-jest-dom
+zsh: First commit: gc -am"First Commit" && gp
+
+
